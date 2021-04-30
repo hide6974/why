2cd5969eb720ea374e17a0eb3974cf093d1f3c8f
hide6974 <hide6974@gmail.com>
2021-05-01 08:09:05 +0900

    first comiit

--- viewlist/README.md
+++ viewlist/README.md
@@ -1,4 +1,4 @@
-# why
+# viewlist
 
 > A Vue.js project
 
@@ -19,7 +19,3 @@ npm run build --report
 ```
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
-
-# how deploy
-npm run build 
-firebase deploy
---
+++ viewlist/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="root">
+    <APIcall class="APIcall" @loadStart="onLoadStart" @loadComplete="onLoadComplete"/>
+    <Result :items="items" :loadProgress="loadProgress" ></Result>
+  </div>
+</template>
+
+<script>
+// Import Part
+import APIcall from '@/components/APIcall';
+import Result from '@/components/Result';
+
+export default {
+  components:{
+    APIcall,
+    Result
+  },
+  data() {
+    return {
+      items: [],
+      loadProgress: false,
+    };
+  },
+  methods:{
+    onLoadStart(){
+      this.loadProgress = true;
+    },
+    onLoadComplete({ results }) {
+      this.items = results;
+      this.loadProgress = false;
+    },
+  },
+}
+
+</script>
+<style>
+html {
+  font-size: 62.5%;
+}
+
+body{
+  margin: 0;
+}
+
+a{
+  text-decoration: none;
+}
+
+p{
+  margin: 0;
+}
+
+ul{
+  padding: 0;
+  margin: 0;
+}
+</style>
